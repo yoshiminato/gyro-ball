6ed729832e1b1d4f06c8e74943b369db6a495f87
yoshi <c081ffw@yamaguchi-u.ac.jp>
2026-06-29 18:13:02 +0900

    viteを導入し、開発サーバー環境を構築

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+export default defineConfig({
+  plugins: [basicSsl()],
+  server: {
+    host: true,
+    https: true,
+  },
+})
