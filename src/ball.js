7729b4c51da7397050bf68bcf2d88c3ae04ce6de
yoshi <c081ffw@yamaguchi-u.ac.jp>
2026-07-02 14:06:08 +0900

    ボールのメッシュ生成ロジックの分離

--- src/ball.js
+++ src/ball.js
@@ -1,5 +1,8 @@
 import { gyroEnabled, calculateForceFromGyro} from './input/gyro.js';
 import { calculateForceFromKeys } from './input/keyboard.js';
+import { createBallBody } from './core/physics.js';
+import { createBallMesh } from './core/renderer.js';
+
 
 export class Ball{
 
@@ -8,8 +11,13 @@ export class Ball{
     static HEADING_SCALE = 2.0;
     static MAX_VEL = 15;
 
-    constructor(ballBody){ 
-        this.body = ballBody;
+    initialPosition = { x: 0, y: Ball.BALL_R + 1, z: 0 };
+
+    constructor(){ 
+
+        // ボール生成(物理エンジン側)
+        this.body = createBallBody(Ball.BALL_R);
+        this.mesh = createBallMesh(Ball.BALL_R);
 
         // ボールの向き
         this.heading = 0;
@@ -23,12 +31,29 @@ export class Ball{
         this.heading = 0;
     }
 
+    resetPosition(){
+        this.body.position.set(
+            this.initialPosition.x, 
+            this.initialPosition.y,
+            this.initialPosition.z
+        );
+        this.body.velocity.set(0, 0, 0);
+        this.body.angularVelocity.set(0, 0, 0);
+        this.body.quaternion.set(0, 0, 0, 1);
+    }
+
+    // ジャンプ(簡易実装)
+    triggerJump() {
+        if (this.body.position.y < 2.0) {
+            this.body.velocity.y = 25;
+        }
+    }
+
     calculateForce(dt){
         if (gyroEnabled)           
             calculateForceFromGyro(this, dt);
         else
-            calculateForceFromKeys(this, dt);
-        
+            calculateForceFromKeys(this, dt);      
     }
 
     applyForce() {
