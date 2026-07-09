f64317ebe9ce99d42dc9c0c92d0b0fcb6a34e88c
yoshi <c081ffw@yamaguchi-u.ac.jp>
2026-07-09 12:41:22 +0900

    ルータの追加(画面遷移関数をイベントリスナに登録する関数)

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { showTitlePage } from './ui/titlePage.js';
 import { showModeSelectPage } from './ui/modeSelectPage';
+import { showTutorialSkipPage } from './ui/tutorialSkipPage.js';
 
 export function registerRouterEvents() {
     window.addEventListener('load', showTitlePage);
     window.addEventListener('title-exit', showModeSelectPage);
+    window.addEventListener('tutorial-skip-trigger', showTutorialSkipPage);
 }
 
